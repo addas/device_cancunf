8080808ba011c7f6592ffeb0da4af29d4dd94c5b
addas <addas.97@live.com>
2024-11-05 20:44:21 +0400

    Change aosp_ to twrp_ in twrp_cancunf.mk

--- twrp_cancunf.mk
+++ twrp_cancunf.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/motorola/cancunf/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_cancunf
+PRODUCT_NAME := twrp_cancunf
 PRODUCT_DEVICE := cancunf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
